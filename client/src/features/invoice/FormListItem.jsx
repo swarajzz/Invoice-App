0e9f26b287c2746ebe513ad61dcbf48cb0a4a16d
Swaraj <sharma.swaraj0403@gmail.com>
2024-05-22 10:07:38 +0530

    Set total value based on qty and price

--- client/src/features/invoice/FormListItem.jsx
+++ client/src/features/invoice/FormListItem.jsx
@@ -1,8 +1,19 @@
 import FormRow from "../../ui/FormRow";
 import styles from "../styles/FormListItem.module.scss";
 import deleteIcon from "../../assets/icon-delete.svg";
+import { useEffect, useState } from "react";
 
 function FormListItem() {
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [total, setTotal] = useState(0.0);
+
+  useEffect(() => {
+    let value = quantity * price;
+    value = parseFloat(value).toFixed(2);
+    setTotal(value);
+  }, [quantity, price]);
+
   return (
     <li className={styles.listItem}>
       <div className={styles.firstBox}>
@@ -11,13 +22,31 @@ function FormListItem() {
         </FormRow>
       </div>
       <FormRow label={"Qty."}>
-        <input type="text" id="quantity" />
+        <input
+          onChange={(e) => setQuantity(e.target.value)}
+          defaultValue={1}
+          type="number"
+          min={0}
+          id="quantity"
+        />
       </FormRow>
       <FormRow label={"Price"}>
-        <input type="text" id="price" />
+        <input
+          onChange={(e) => setPrice(e.target.value)}
+          type="number"
+          id="price"
+          defaultValue={0}
+          min={0}
+        />
       </FormRow>
       <FormRow label={"Total"}>
-        <input className={styles.total} type="text" id="total" defaultValue={100} disabled/>
+        <input
+          className={styles.total}
+          type="number"
+          id="total"
+          value={total}
+          disabled
+        />
       </FormRow>
       <button className={styles.iconBox}>
         <img className={styles.deleteIcon} src={deleteIcon} alt="Delete Icon" />
