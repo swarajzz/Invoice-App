eb44e59dedc7f589672e172a3aa16b7db8338ce1
Swaraj <sharma.swaraj0403@gmail.com>
2024-06-12 17:25:18 +0530

    Handle zero invoices design

--- client/src/features/invoice/InvoiceList.jsx
+++ client/src/features/invoice/InvoiceList.jsx
@@ -2,6 +2,7 @@ import InvoiceItem from "./InvoiceItem";
 import styles from "../styles/InvoiceList.module.scss";
 import Spinner from "../../ui/Spinner";
 import NotFound from "../../ui/Error";
+import empty from "../../assets/empty.svg";
 
 function InvoiceList({ toggleFilterValues, invoices, isLoading, error }) {
   if (isLoading) return <Spinner />;
@@ -13,11 +14,25 @@ function InvoiceList({ toggleFilterValues, invoices, isLoading, error }) {
 
   return (
     <>
-      <ul className={styles.invoiceList}>
-        {(toggleFilterValues.length ? newInvoices : invoices).map((invoice) => (
-          <InvoiceItem key={invoice.id} invoice={invoice} />
-        ))}
-      </ul>
+      {invoices.length ? (
+        <ul className={styles.invoiceList}>
+          {(toggleFilterValues.length ? newInvoices : invoices).map(
+            (invoice) => (
+              <InvoiceItem key={invoice.id} invoice={invoice} />
+            )
+          )}
+        </ul>
+      ) : (
+        <div className={styles.empty_box}>
+          <img src={empty} className={styles.empty_img} />
+
+          <h4>There is nothing here</h4>
+
+          <p>
+            Create a new invoice by clicking the <strong>New Invoice</strong> button and get started
+          </p>
+        </div>
+      )}
     </>
   );
 }
