02f17a41bd912848edadbd6c471aa1b7ef06fd76
Swaraj <sharma.swaraj0403@gmail.com>
2024-06-13 09:58:23 +0530

    Fix receipt responsive design

--- client/src/features/invoice/InvoiceReceipt.jsx
+++ client/src/features/invoice/InvoiceReceipt.jsx
@@ -6,6 +6,8 @@ function InvoiceReceipt({ items }) {
     0
   );
 
+  console.log(items)
+
   return (
     <div className={styles.receipt_container}>
       <div className={styles.wrapper}>
@@ -21,26 +23,28 @@ function InvoiceReceipt({ items }) {
 
           <tbody>
             {items.map((item) => (
-                <tr key={item.name}>
-                  <td className={styles.itemName}>{item.name}</td>
-                  <td>{item.quantity}</td>
-                  <td>$ {item.price}</td>
-                  <td className={styles.total}>
-                    $ {item.quantity * item.price}
-                  </td>
-                </tr>
+              <tr key={item.name}>
+                <td className={styles.itemName}>{item.name}</td>
+                <td>{item.quantity}</td>
+                <td>$ {item.price}</td>
+                <td className={styles.total}>$ {item.quantity * item.price}</td>
+              </tr>
             ))}
           </tbody>
         </table>
         <div className={styles.phoneResponsive}>
-          <div>
-            <p className={styles.itemName}>Website Redesign</p>
-            <p className={styles.quantity}>1 x $ 1800</p>
-          </div>
+          {items.map((item) => (
+            <div className={styles.phoneItems} key={item._id}>
+              <div>
+                <p className={styles.itemName}>{item.name}</p>
+                <p className={styles.quantity}>1 x $ {item.price}</p>
+              </div>
 
-          <div>
-            <p className={styles.itemName}>$ 1800</p>
-          </div>
+              <div>
+                <p className={styles.itemName}>$ {item.total}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
